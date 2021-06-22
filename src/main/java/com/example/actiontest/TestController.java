7cee0cd1284b34f2107864c01852044a0876da04
platanus <platanus@canxan.com>
2021-06-22 23:58:16 +0900

    add spring boot project

---
+++ src/main/java/com/example/actiontest/TestController.java
@@ -0,0 +1,15 @@
+package com.example.actiontest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping(value="/")
+    public String test() {
+        String tests = "hello world";
+        System.out.println(tests);
+        return tests;
+    }
+}
